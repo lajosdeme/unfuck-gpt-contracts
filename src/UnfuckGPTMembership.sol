ca92eb95cde85e93ce0919a9cfc5ada9a7161b99
lajosdeme <lajosd@protonmail.ch>
2023-10-28 10:47:33 +0100

    added price data contract

--- src/UnfuckGPTMembership.sol
+++ src/UnfuckGPTMembership.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract UnfuckGPTMembership {
-    
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract UnfuckGPTMembership is ERC721 {
+    constructor() ERC721("Unfuck GPT Membership NFT", "!fck") { }
 }
