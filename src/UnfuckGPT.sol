340301b5392402b3365c824d193ac88b879c861e
lajosdeme <lajosd@protonmail.ch>
2023-10-28 12:07:50 +0100

    implement crypto price tracking with flare

--- src/UnfuckGPT.sol
+++ src/UnfuckGPT.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+/* 
+Chain ID 4: BSC Testnet 0x80aC94316391752A193C1c47E27D382b507c93F3
+Chain ID 5: Polygon Testnet 0x0591C25ebd0580E0d4F27A82Fc2e24E7489CB5e0
+Chain ID 6: Avalanche Testnet (Fuji) 0xA3cF45939bD6260bcFe3D66bc73d60f19e49a8BB
+Chain ID 14: Celo Testnet: 0x306B68267Deb7c5DfCDa3619E22E9Ca39C374f84
+Chain ID 16: Moonbeam Testnet 0x0591C25ebd0580E0d4F27A82Fc2e24E7489CB5e0
+ */
+
 contract UnfuckGPT {
-    
+
 }
--- src/UnfuckGPTPriceData.sol
+++ src/UnfuckGPTPriceData.sol
@@ -1,6 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract UnfuckGPTPriceData {
-    
+import "@flarenetwork/flare-periphery-contracts/coston2/util-contracts/userInterfaces/IFlareContractRegistry.sol";
+import "@flarenetwork/flare-periphery-contracts/coston2/ftso/userInterfaces/IFtsoRegistry.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/* 
+Supported symbols: 
+[( XRP), (LTC), (XLM), (DOGE), (ADA), (ALGO), (BTC), (ETH), (FIL), (FLR), (ARB), (AVAX), (BNB), (MATIC), (SOL), (USDC), (USDT), (XDC)]
+ */
+contract UnfuckGPTPriceData is Ownable {
+    address private constant FLARE_CONTRACT_REGISTRY =
+        0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019;
+
+    IFlareContractRegistry contractRegistry =
+        IFlareContractRegistry(FLARE_CONTRACT_REGISTRY);
+
+    IFtsoRegistry.PriceInfo[] public currentPriceInfo;
+
+    constructor() Ownable(msg.sender) {}
+
+    function refreshPrices() external onlyOwner {
+        IFtsoRegistry ftsoRegistry = IFtsoRegistry(
+            contractRegistry.getContractAddressByName("FtsoRegistry")
+        );
+
+        IFtsoRegistry.PriceInfo[] memory _priceInfos = ftsoRegistry
+            .getAllCurrentPrices();
+
+        for (uint256 i = 0; i < _priceInfos.length; i++) {
+            currentPriceInfo[i] = _priceInfos[i];
+        }
+    }
+
+    function priceInfosLength() external view returns (uint256) {
+        return currentPriceInfo.length;
+    }
+
+    function latestPriceForToken(
+        string memory _symbol
+    ) external view returns (IFtsoRegistry.PriceInfo memory) {
+         IFtsoRegistry ftsoRegistry = IFtsoRegistry(
+            contractRegistry.getContractAddressByName("FtsoRegistry")
+        );
+        uint256 _idx = ftsoRegistry.getFtsoIndex(_symbol);
+        return currentPriceInfo[_idx];
+    }
 }
